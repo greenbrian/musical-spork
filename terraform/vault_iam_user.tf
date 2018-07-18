b58f70ab1a028d1797f58ccaa4ac4dc659739857
Brian Green <greenbrian@gmail.com>
2018-07-18 13:13:29 -0500

    forgot some files

---
+++ terraform/vault_iam_user.tf
@@ -0,0 +1,30 @@
+resource "aws_iam_user" "vault_validation" {
+  name = "vault_validation"
+  path = "/${random_id.environment_name.hex}-east/"
+}
+
+resource "aws_iam_access_key" "vault" {
+  user = "${aws_iam_user.vault_validation.name}"
+}
+
+resource "aws_iam_user_policy" "vault_ro" {
+  name = "vault_validation_policy"
+  user = "${aws_iam_user.vault_validation.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:DescribeInstances",
+        "iam:GetRole",
+        "iam:GetUser"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
