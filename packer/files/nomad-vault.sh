100b664f2a5d3ee19dd9023f24bb0521bf9804b8
Austin Workman <austin@hashicorp.com>
2018-10-12 00:54:30 -0500

    Moving Vault Token into an environment variable to avoid exposure via ps/systemctl

--- packer/files/nomad-vault.sh
+++ packer/files/nomad-vault.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ -f /mnt/ramdisk/token ]; then 
-  exec /usr/local/bin/nomad agent \
-  -config=/etc/nomad.d \
-  -vault-token=$(vault unwrap -field=token $(jq -r '.token' /mnt/ramdisk/token)) \
-  -vault-tls-skip-verify=true
+if [ -f /mnt/ramdisk/token ]; then
+  exec env VAULT_TOKEN=$(vault unwrap -field=token $(jq -r '.token' /mnt/ramdisk/token)) \
+    /usr/local/bin/nomad agent \
+      -config=/etc/nomad.d \
+      -vault-tls-skip-verify=true
 else
   echo "Nomad service failed due to missing Vault token"
   exit 1
