3c3f44b155f04da18fd09cf0f0dfcb2df84c9544
Austin Workman <austin@hashicorp.com>
2018-10-31 13:17:35 -0500

    Moving Vault Token into an environment variable to avoid exposure via ps/systemctl (#27)

--- packer/files/consul-template-vault.sh
+++ packer/files/consul-template-vault.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if [ -f /mnt/ramdisk/token ]; then
-  exec /usr/local/bin/consul-template \
-    -config=/etc/consul-template.d \
-    -vault-token=$(vault unwrap -field=token $(jq -r '.token' /mnt/ramdisk/token)) \
-    -vault-ssl-verify=false
+  exec env VAULT_TOKEN=$(vault unwrap -field=token $(jq -r '.token' /mnt/ramdisk/token)) \
+      /usr/local/bin/consul-template \
+        -config=/etc/consul-template.d \
+        -vault-ssl-verify=false
 else
   echo "Consul-template service failed due to missing Vault token"
   exit 1
